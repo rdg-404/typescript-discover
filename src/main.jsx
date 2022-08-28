22787cb3bec36c37b949634184e387d2e9b4324d
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-08-27 23:37:42 -0300

    ADD CSS AND ORGANIZE THE FOLDERS

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './Home'
+import { Home } from './pages/Home'
+import './style.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import './home.css'
+
+export function Home() {
+
+  return (
+    <div className="container">
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder='Digite seu nome...' />
+      <button type='button'>Adicionar</button>
+    </div>
+  )
+}
+
+
