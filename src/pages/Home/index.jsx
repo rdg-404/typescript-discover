90dae124bbb9d8f203fe3afb9f381c556b025a1d
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-08-28 20:58:10 -0300

    use state

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,21 @@
 import './home.css';
 import { Card } from "../../components/Card";
+import { useState } from 'react';
+import React from 'react';
 
 
 export function Home() {
 
+  const [studentName, setStudentName] = useState();
+
   return (
     <div className="container">
-      <h1>Lista de Presença</h1>
-      <input type="text" placeholder='Digite seu nome...' />
+      <h1>Nome: {studentName}</h1>
+      <input 
+        type="text" 
+        placeholder='Digite seu nome...' 
+        onChange={e => setStudentName(e.target.value)}
+      />
       <button type='button'>Adicionar</button>
       <Card name="Rodrigo" time="14:35"/>
       <Card name="Ana" time="14:45"/>
